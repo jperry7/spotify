fdd663e92bea88fd86f5f6fb6b916f87fb0671e8
Joshua Perry <jperry7@uh.edu>
2019-01-03 08:13:33 -0800

    Updates

---
+++ precache-manifest.c97da7c7baed29b40342e891d5acbf3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb90536c20586cad7eec",
+    "url": "/spotify/static/js/runtime~main.bb90536c.js"
+  },
+  {
+    "revision": "372a958224ae76fd06c7",
+    "url": "/spotify/static/js/main.372a9582.chunk.js"
+  },
+  {
+    "revision": "e6c1364db654ac3c898e",
+    "url": "/spotify/static/js/1.e6c1364d.chunk.js"
+  },
+  {
+    "revision": "372a958224ae76fd06c7",
+    "url": "/spotify/static/css/main.a68455d7.chunk.css"
+  },
+  {
+    "revision": "7daf823617ca21b14cacf80219b4d250",
+    "url": "/spotify/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64c4cd32fd0c8b3023cfe44a38902d58.js"
+  "/spotify/precache-manifest.c97da7c7baed29b40342e891d5acbf3b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/spotify/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
