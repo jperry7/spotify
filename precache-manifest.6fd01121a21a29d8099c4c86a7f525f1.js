1d437391a1fd6177a305a740a0b580d942b41919
Joshua Perry <jperry7@uh.edu>
2019-01-03 07:44:11 -0800

    Updates

---
+++ precache-manifest.6fd01121a21a29d8099c4c86a7f525f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb90536c20586cad7eec",
+    "url": "/spotify/static/js/runtime~main.bb90536c.js"
+  },
+  {
+    "revision": "91225475e0eeaa495d22",
+    "url": "/spotify/static/js/main.91225475.chunk.js"
+  },
+  {
+    "revision": "e6c1364db654ac3c898e",
+    "url": "/spotify/static/js/1.e6c1364d.chunk.js"
+  },
+  {
+    "revision": "91225475e0eeaa495d22",
+    "url": "/spotify/static/css/main.a68455d7.chunk.css"
+  },
+  {
+    "revision": "99b36c25832932535980c8ffd4bc3e6a",
+    "url": "/spotify/index.html"
+  }
+];
